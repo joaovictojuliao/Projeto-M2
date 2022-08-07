76c06997acd877e1622f2ad923fe62a14e2abb87
camillaruwel <107160555+camillaruwel@users.noreply.github.com>
2022-08-06 21:47:07 -0300

    Adicionando dicionário data e hora.

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import Classe2 as cl
 import datetime as date
 
-lista_nome=[]
 rodando=True
+lista_nome=[]
 lista_idade = []
 lista_genero = []
 pergunta1 = []
 pergunta2 = []
 pergunta3 = []
 pergunta4 = []
+dataEHora = []
 dict = {
-    'nomes':lista_nome,
-    'idades':lista_idade,
-    'generos':lista_genero,
-    'pergunta1':pergunta1,
-    'pergunta2':pergunta2,
-    'pergunta3':pergunta3,
-    'pergunta4':pergunta4    
+    'Nome':lista_nome,
+    'Idade':lista_idade,
+    'Gênero':lista_genero,
+    'pergunta-1':pergunta1,
+    'pergunta-2':pergunta2,
+    'pergunta-3':pergunta3,
+    'pergunta-4':pergunta4,
+    'Data e hora':dataEHora   
 } 
 
 while rodando:       
@@ -24,7 +26,6 @@ while rodando:
     idade = cl.PesquisaSaude.idadeVerif(cl.PesquisaSaude, idade)
     if idade == 0:
         print('Programa finalizado')
-        print(dict)
         break
     nome=input('Digite o nome do entrevistado: ')
     lista_idade.append(idade)
@@ -36,3 +37,6 @@ while rodando:
     pergunta2.append(perguntas[1])
     pergunta3.append(perguntas[2])
     pergunta4.append(perguntas[3])
+    dataHoraCadastrada = date.datetime.now()
+    dataHoraCadastrada = dataHoraCadastrada.strftime('%d/%m/%Y %H:%M:%S')
+    dataEHora.append(dataHoraCadastrada) 
