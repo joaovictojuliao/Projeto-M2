eb6ee0ddd2503e9f82efb2a7a7f08c2745cf71e4
camillaruwel <107160555+camillaruwel@users.noreply.github.com>
2022-08-09 18:12:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import Classe2 as cl
-import datetime as date
-
+pesquisa=0
+cont=1
 rodando=True
 lista_nome=[]
 lista_idade = []
@@ -11,32 +11,40 @@ pergunta3 = []
 pergunta4 = []
 dataEHora = []
 dict = {
-    'Nome':lista_nome,
-    'Idade':lista_idade,
-    'Gênero':lista_genero,
-    'pergunta-1':pergunta1,
-    'pergunta-2':pergunta2,
-    'pergunta-3':pergunta3,
-    'pergunta-4':pergunta4,
-    'Data e hora':dataEHora   
+    'Nome ':lista_nome,
+    'Idade ':lista_idade,
+    'Genero ':lista_genero,
+    'Pergunta-1 ':pergunta1,
+    'Pergunta-2 ':pergunta2,
+    'Pergunta-3 ':pergunta3,
+    'Pergunta-4 ':pergunta4,
+    'Data e hora ':dataEHora   
 } 
 
-while rodando:       
-    idade = 1
-    idade = cl.PesquisaSaude.idadeVerif(cl.PesquisaSaude, idade)
-    if idade == 0:
-        print('Programa finalizado')
-        break
-    nome=input('Digite o nome do entrevistado: ')
-    lista_idade.append(idade)
-    genero=cl.PesquisaSaude.generos(cl.PesquisaSaude)
-    lista_genero.append(genero)
-    perguntas=cl.PesquisaSaude.quatroPerguntas(cl.PesquisaSaude, 0,0,0,0)
-    lista_nome.append(nome)
-    pergunta1.append(perguntas[0])
-    pergunta2.append(perguntas[1])
-    pergunta3.append(perguntas[2])
-    pergunta4.append(perguntas[3])
-    dataHoraCadastrada = date.datetime.now()
-    dataHoraCadastrada = dataHoraCadastrada.strftime('%d/%m/%Y %H:%M:%S')
-    dataEHora.append(dataHoraCadastrada) 
+pesquisa=cl.PesquisaSaude.inicio(cl.PesquisaSaude, pesquisa)
+if cl.PesquisaSaude.inicio(cl.PesquisaSaude, pesquisa)==1:
+    while rodando:
+        nome=input(f'Digite o nome do {cont}º entrevistado: ') 
+        idade = 1
+        idade = cl.PesquisaSaude.idadeVerif(cl.PesquisaSaude, idade)
+        if idade == 0:
+            print('\n\n**************************PROGRAMA FINALIZADO**************************\n')
+            cl.PesquisaSaude.arquivoCsv(cl.PesquisaSaude,dict)
+            break
+       
+        genero=cl.PesquisaSaude.generos(cl.PesquisaSaude)    
+        perguntas=cl.PesquisaSaude.quatroPerguntas(cl.PesquisaSaude, 0,0,0,0)
+        dt=cl.PesquisaSaude.dataHora(cl.PesquisaSaude)
+
+        lista_idade.append(idade)
+        lista_nome.append(nome)
+        lista_genero.append(genero)
+        pergunta1.append(perguntas[0])
+        pergunta2.append(perguntas[1])
+        pergunta3.append(perguntas[2])
+        pergunta4.append(perguntas[3])
+        dataEHora.append(dt)
+        cont+=1
+else:
+    print('\n\n**************************PROGRAMA FINALIZADO**************************\n')
+    
