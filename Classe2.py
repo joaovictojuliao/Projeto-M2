3c20865fd5c3fe6a8ee3cef965bd341814ac001d
João Victor Julião <107512421+joaovictojuliao@users.noreply.github.com>
2022-08-09 16:11:25 -0300

    Convertendo CSV, organizando código e outros detalhes.

--- Classe2.py
+++ Classe2.py
@@ -1,80 +1,104 @@
+import datetime as date
 import pandas as pd
 
-class PesquisaSaude():
-    def __init__(self, nome, idade, datahora):
-        self.nome=nome
-        self.idade=idade
-        self.datahora=datahora
+class PesquisaSaude():      
+    def inicio(self, pesquisa):
+        while pesquisa>2 or pesquisa<1:
+            pesquisa=int(input('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - '
+              '\nOlá, bem-vindo a pesquisa sobre a "Conscientização da Saúde Mental"'
+              '\n---------------- Deseja participar da pesquisa ? ----------------'
+              '\n[ 1 ]- Sim'
+              '\n[ 2 ]- Nao\n'))
+        return pesquisa
 
-    def idadeVerif(self, idade):        
-        while idade<0 or idade>140:
-            idade=int(input("Digite a idade correta do entrevistado, lembrando precisa ser maior de idade: "))
+    def idadeVerif(self, idade):
+        while (idade<18 or idade>140) and idade != 0:
+            idade=int(input(f'\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - '
+            '\nDigite a idade do entrevistado, lembrando precisa ser maior de idade: '))
         return idade
 
     def generos(self):
-        genero = int(input('Digite o número que corresponde ao seu gênero: '
-                '\n1- Masculino\n2- Feminino\n3- Binário\n4- Outro\nDigite aqui: '))
+        genero = int(input('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+                '\nDigite o número que corresponde ao gênero do entrevistado: '
+                '\n[ 1 ]- Masculino\n[ 2 ]- Feminino\n[ 3 ]- Binário\n[ 4 ]- Outro\nDigite aqui: '))
         while genero<1 or genero>4:
             print('Valor inválido,tente novamente...')
             genero=int(input('Digite o número que corresponde ao seu gênero: '
-                '\n1- Masculino\n2- Feminino\n3- Binário\n4- Outro\nDigite aqui: '))
+                '\n[ 1 ]- Masculino\n[ 2 ]- Feminino\n[ 3 ]- Binário\n[ 4 ]- Outro\nDigite aqui: '))
         if genero==1:
-            self.genero='Masculino'
+            self.genero='Masculino '
             return self.genero
         elif genero==2:
-            self.genero='Feminino'
+            self.genero='Feminino '
             return self.genero
         elif genero==3:
-            self.genero='Binário'
+            self.genero='Binario '
             return self.genero
         elif genero==4:
-            self.genero='Outro'
+            self.genero='Outro '
             return self.genero
     
     def quatroPerguntas(self, pergunta1, pergunta2, pergunta3, pergunta4):
-
         while pergunta1<1 or pergunta1>3:
-            pergunta1=int(input('1- Você acha importante cuidar da saúde mental?: '))
+            print('\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+            pergunta1=int(input('1- Você acha importante cuidar da saúde mental?: '
+            '\n[ 1 ]- Sim\n[ 2 ]- Nao\n[ 3 ]- Nao sei responder\n'))
             if pergunta1==1:
-                self.resposta1='Sim'
+                self.resposta1='Sim '
             elif pergunta1==2:
-                self.resposta1='Não'
+                self.resposta1='Nao '
             elif pergunta1==3:
-                self.resposta1='Não sei responder'
+                self.resposta1='Nao sei responder '
             else:
                 print('Resposta inválida')
 
         while pergunta2<1 or pergunta2>3:
-            pergunta2=int(input('2- Você sabe qual a utilidade do acompanhamento psicológico?: '))
+            print('\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+            pergunta2=int(input('2- Você sabe qual a utilidade do acompanhamento psicológico?: '
+            '\n[ 1 ]- Sim\n[ 2 ]- Nao\n[ 3 ]- Nao sei responder\n'))
             if pergunta2==1:
-                self.resposta2='Sim'
+                self.resposta2='Sim '
             elif pergunta2==2:
-                self.resposta2='Não'
+                self.resposta2='Nao '
             elif pergunta2==3:
-                self.resposta2='Não sei responder'
+                self.resposta2='Nao sei responder '
             else:
                 print('Resposta inválida')
 
         while pergunta3<1 or pergunta3>3:
-            pergunta3=int(input('3- Você sente a necessidade de ter acompanhamento psicológico?: '))
+            print('\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+            pergunta3=int(input('3- Você sente a necessidade de ter acompanhamento psicológico?: '
+            '\n[ 1 ]- Sim\n[ 2 ]- Nao\n[ 3 ]- Nao sei responder\n'))
             if pergunta3==1:
-                self.resposta3='Sim'
+                self.resposta3='Sim '
             elif pergunta3==2:
-                self.resposta3='Não'
+                self.resposta3='Nao '
             elif pergunta3==3:
-                self.resposta3='Não sei responder'
+                self.resposta3='Nao sei responder '
             else:
-                print('Resposta inválida')
+                print('Resposta invalida')
 
         while pergunta4<1 or pergunta4>3:
-            pergunta4=int(input('4- Você possui acompanhamento psicológico?: '))
+            print('\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+            pergunta4=int(input('4- Você possui acompanhamento psicológico?: '
+            '\n[ 1 ]- Sim\n[ 2 ]- Nao\n[ 3 ]- Nao sei responder\n'))
             if pergunta4==1:
-                self.resposta4='Sim'
+                self.resposta4='Sim '
             elif pergunta4==2:
-                self.resposta4='Não'
+                self.resposta4='Nao '
             elif pergunta4==3:
-                self.resposta4='Não sei responder'
+                self.resposta4='Nao sei responder '
             else:
                 print('Resposta inválida')
-
         return self.resposta1, self.resposta2, self.resposta3, self.resposta4
+
+    def dataHora(self):
+        dataHoraCadastrada = date.datetime.now()
+        dataHoraCadastrada = dataHoraCadastrada.strftime('%d/%m/%Y %H:%M:%S')
+        return dataHoraCadastrada
+
+    def arquivoCsv(self, dict):
+        df = pd.DataFrame(dict)
+        df.to_csv("perguntas.csv", index = False, sep ='/')
+        print('****Foi criado um arquivo (perguntas.csv) com as informações obtidas!****' 
+        '\n- - - - - - - - Obrigado por participar da nossa pesquisa - - - - - - - -\n\n\n')
